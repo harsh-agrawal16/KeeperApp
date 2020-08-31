cd62bbf12bfa95c5e0ebb0308b9314960c10aaf9
harsh-agrawal16 <agrwl.harsh@gmail.com>
2020-08-31 15:53:21 +0530

    First Commit

---
+++ src/components/Note.jsx
@@ -0,0 +1,14 @@
+import React from "react"
+
+function Note(){
+    return <div className = "note">
+        <h1>
+            Title of the keep.
+        </h1>
+        <p>
+            Paragraph of the keep.
+        </p>
+    </div>;
+}
+
+export default Note;
